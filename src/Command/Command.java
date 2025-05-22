400bf1d7b41697580a04502be4015db87a6eb586
Gabriel de Jesus <69818016+Zoreta-create@users.noreply.github.com>
2025-05-22 03:02:35 +0000

    mudanças na interface Command

--- src/Command/Command.java
+++ src/Command/Command.java
@@ -1,3 +1,7 @@
+/* Comando é um padrão de design comportamental que transforma uma solicitação em um objeto independente que 
+contém todas as informações sobre ela. Essa transformação permite passar solicitações como argumentos de método, 
+atrasar ou enfileirar a execução de uma solicitação e oferecer suporte a operações que podem ser desfeitas.*/
+
 package Command;
 
 public interface Command {
